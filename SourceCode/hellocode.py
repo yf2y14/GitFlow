8996b6c19ef3fa6bfa1ef62a36172173eed82313
mashroom16 <mashroom16@gmail.com>
2023-08-21 17:17:46 +0800

    why~~

--- SourceCode/hellocode.py
+++ SourceCode/hellocode.py
@@ -1,6 +1,8 @@
 print("Hello everyone in team")
 
-my_baby_bool = "false"
+my_baby_bool = "true"
 print (type(my_baby_bool))
 my_baby_bool_two = "true"
 print (type(my_baby_bool_two))
+
+print("why why why")
