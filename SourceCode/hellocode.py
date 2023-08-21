8c65f10668ccbf380a37bda0d2c2f838d5341d9e
mashroom16 <mashroom16@gmail.com>
2023-08-21 17:26:48 +0800

    pei

--- SourceCode/hellocode.py
+++ SourceCode/hellocode.py
@@ -8,3 +8,12 @@ my_baby_bool_two = "true"
 print (type(my_baby_bool_two))
 
 print("why why why")
+
+
+
+
+# Enter a user name here, make sure to make it a string
+user_name = "Dave"
+
+if user_name == "Dave":
+  print("Get off my computer Dave!")
